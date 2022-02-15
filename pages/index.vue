d3e3be33b8e31bc31d8ab72589929de42a7ebb2c
[NayeemHR] <nayeemhasan042@yahoo.com>
2022-02-15 21:54:31 +0600

    v6

--- pages/index.vue
+++ pages/index.vue
@@ -49,10 +49,81 @@
       </div>
       </vue-slick-carousel>
 
-      <div class="container">
-        
-      </div>
+      
     </div>
+    <div class="container">
+        <h3 class="text-4xl mb-6">Search by Category</h3>
+        <div class="flex item-center">
+          <div style="height: 3px" class="w-32 hs-dark-green-bg"></div>
+          <div style="height: 2px" class="w-full bg-gray-200"></div>
+        </div>
+        <vue-slick-carousel class="category-carousel" v-bind="categoryCarouselSettings">
+          <div class="bg-gray-200 p-4 mx-4">
+            <div class="h-32 flex justify-center items-center">
+              <img src="~/assets/img/carousel-img-1.png" alt="">
+            </div>
+            <h4 class="text-xl">Groceries</h4>
+          </div>
+          <div class="bg-gray-200 p-4 mx-4">
+            <div class="h-32 flex justify-center items-center">
+              <img src="~/assets/img/carousel-img-1.png" alt="">
+            </div>
+            <h4 class="text-xl">Groceries</h4>
+          </div>
+          <div class="bg-gray-200 p-4 mx-4">
+            <div class="h-32 flex justify-center items-center">
+              <img src="~/assets/img/carousel-img-1.png" alt="">
+            </div>
+            <h4 class="text-xl">Groceries</h4>
+          </div>
+          <div class="bg-gray-200 p-4 mx-4">
+            <div class="h-32 flex justify-center items-center">
+              <img src="~/assets/img/carousel-img-1.png" alt="">
+            </div>
+            <h4 class="text-xl">Groceries</h4>
+          </div>
+          <div class="bg-gray-200 p-4 mx-4">
+            <div class="h-32 flex justify-center items-center">
+              <img src="~/assets/img/carousel-img-1.png" alt="">
+            </div>
+            <h4 class="text-xl">Groceries</h4>
+          </div>
+          <div class="bg-gray-200 p-4 mx-4">
+            <div class="h-32 flex justify-center items-center">
+              <img src="~/assets/img/carousel-img-1.png" alt="">
+            </div>
+            <h4 class="text-xl">Groceries</h4>
+          </div>
+          <div class="bg-gray-200 p-4 mx-4">
+            <div class="h-32 flex justify-center items-center">
+              <img src="~/assets/img/carousel-img-1.png" alt="">
+            </div>
+            <h4 class="text-xl">Groceries</h4>
+          </div>
+          <div class="bg-gray-200 p-4 mx-4">
+            <div class="h-32 flex justify-center items-center">
+              <img src="~/assets/img/carousel-img-1.png" alt="">
+            </div>
+            <h4 class="text-xl">Groceries</h4>
+          </div>
+          <div class="bg-gray-200 p-4 mx-4">
+            <div class="h-32 flex justify-center items-center">
+              <img src="~/assets/img/carousel-img-1.png" alt="">
+            </div>
+            <h4 class="text-xl">Groceries</h4>
+          </div>
+          <template #prevArrow="arrowOption">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+            </svg>
+          </template>
+          <template #nextArrow="arrowOption">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            </svg>
+          </template>
+        </vue-slick-carousel>
+      </div>
   </div>
 </template>
 
@@ -61,6 +132,13 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 export default {
   name: 'IndexPage',
-  components: {Header, Footer}
+  components: {Header, Footer},
+  data(){
+    return{
+      categoryCarouselSettings:{
+        "slidesToShow" : 6,
+      }
+    }
+  }
 }
 </script>
